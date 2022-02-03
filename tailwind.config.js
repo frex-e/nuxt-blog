e138c3d11e4ecb3837a3b807466b5317613f662f
Indra Kusumah-kasim <shadeditor@gmail.com>
2022-02-03 22:55:16 +1100

    fixed tailwind???

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['/**/*{.vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'highlight': '#3b82f6'
+      }
+    },
   },
   variants: {
     extend: {},
